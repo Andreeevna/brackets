ee59ac93fb796e1c643b7539af462038f5f80a17
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2021-02-20 22:27:12 +0300

    fix: new sol

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 !== 0) {
-    return false;
-  }
-  let newStr = str.split('');
-  for(let i = 0; i < newStr.length; i++) {
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < bracketsConfig.length; j++) {
-      if (newStr[i] == bracketsConfig[j][0] && newStr[i+1] == bracketsConfig[j][1]) {
-        newStr.splice(i,2);
-        i--;
+      if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
+        arr.splice(i, 2);
+        i= -1;
       }
     }
   }
-  return newStr.length === 0;
+  return !arr.length;
 }
