e37fb2b2a61be26c41107bd321a079b8d6991ee1
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2021-02-20 20:26:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  let newStr = str.split('');
+  for(let i = 0; i < newStr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (newStr[i] == bracketsConfig[j][0] && newStr[i+1] == bracketsConfig[j][1]) {
+        newStr.splice(i,2);
+        i--;
+      }
+    }
+  }
+  return newStr.length === 0;
 }
